1ff3f8cd54cd8a973404944c224e16f5dcc23fbc
Pedro <pedro7d4@gmail.com>
2025-03-14 19:37:20 -0300

    added zombie sprite and hugemap

--- src/main/java/org/engcomp/Zombicide/Actors/Zombie.java
+++ src/main/java/org/engcomp/Zombicide/Actors/Zombie.java
@@ -1,5 +1,7 @@
 package org.engcomp.Zombicide.Actors;
 
+import javax.swing.*;
+
 public abstract class Zombie extends ActorObj {
     public Zombie() {
         super();
@@ -7,6 +9,7 @@ public abstract class Zombie extends ActorObj {
         this.hasRun = true;
         this.health = 2;
         this.textRepr = "abstract Zombie";
+        this.imgRepr = new ImageIcon("assets/zombies/idle.gif");
     }
 
     @Override
